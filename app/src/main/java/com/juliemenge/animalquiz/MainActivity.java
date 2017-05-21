d9fa344279d6ed7d523b8fec278d673114b6a31e
Julie Menge <juliemenge@gmail.com>
2017-05-21 14:10:57 -0400

    button starts new activity

--- app/src/main/java/com/juliemenge/animalquiz/MainActivity.java
+++ app/src/main/java/com/juliemenge/animalquiz/MainActivity.java
@@ -1,13 +1,42 @@
 package com.juliemenge.animalquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        //TO DO
+        //make all questions required
+        //determine animal based on answers
+        //add a caption to the animal
+        //add a toast "You are a ___"
+
+        //when the submit button is clicked, show the activity with the animal based on your choices
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start the new activity
+                startShowAnimal();
+            }
+        });
+
+    }
+
+    //creating the intent to show the animal activity
+    private void startShowAnimal() {
+        Intent intent = new Intent(this, ShowAnimalActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/juliemenge/animalquiz/ShowAnimalActivity.java
+++ app/src/main/java/com/juliemenge/animalquiz/ShowAnimalActivity.java
@@ -1,7 +1,9 @@
 package com.juliemenge.animalquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ShowAnimalActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ShowAnimalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_animal);
+
+        //get the intent used to start this activity
+        Intent intent = getIntent();
+
+        Toast.makeText(this, "You are a dolphin!", Toast.LENGTH_SHORT).show();
     }
 }
